9b13f4cf5b6509e017db667de9c78fbade8b3648
arun ekambaram <arunvit1730@gmail.com>
2025-04-28 19:46:21 +0530

    changes updated

--- app/utils/retriever.py
+++ app/utils/retriever.py
@@ -1,5 +1,6 @@
 from langchain.vectorstores import FAISS
 from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.retrievers.multi_query import ReciprocalRankFusionRetriever
 from langchain.schema import Document
 from langchain.retrievers import BM25Retriever, EnsembleRetriever
 
@@ -8,16 +9,16 @@ from typing import List
 def get_retriever(chunks: List[Document]):
     embedding_model = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
     vectorstore = FAISS.from_documents(chunks, embedding_model)
-    retriever = vectorstore.as_retriever(search_kwargs={"k": 4})
+    retriever = vectorstore.as_retriever(search_kwargs={"k": 8})
 
 
     bm25_retriever = BM25Retriever.from_documents(chunks)
-    bm25_retriever.k = 4
+    bm25_retriever.k = 8
 
     
-    hybrid_retriever = EnsembleRetriever(
+    hybrid_retriever = ReciprocalRankFusionRetriever(
         retrievers=[retriever, bm25_retriever],
-        weights=[0.5, 0.5]  
+        k=4
     )
 
     return hybrid_retriever
